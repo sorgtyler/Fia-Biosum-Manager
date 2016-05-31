411a91b1b4417647de5b3dc02c69013656b0bb64
Lesley <lbross@pdx.edu>
2016-05-31 16:17:54 -0700

    Address issue #22: Add help to project. Project screen is first screen implemented

---
+++ src/XPSDocumentViewer_xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FIA_Biosum_Manager
+{
+    /// <summary>
+    /// Interaction logic for XPSDocumentViewer.xaml
+    /// </summary>
+    public partial class XPSDocumentViewer : System.Windows.Navigation.NavigationWindow
+    {
+        public XPSDocumentViewer()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Hide();
+            e.Cancel = true;
+        }
+    }
+}
