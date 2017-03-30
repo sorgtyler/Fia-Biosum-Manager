edaf4c02e259c23ab55256edfdd1dc16249b6148
Lesley <lbross@pdx.edu>
2017-03-30 11:52:16 -0700

    issue #75 Only adjust tpa if original value > 0

--- src/processor.cs
+++ src/processor.cs
@@ -650,7 +650,7 @@ namespace FIA_Biosum_Manager
                             dblSmLogHwdPct = nextStand.SmLogHwdVolCfPa / nextStand.SmLogVolCfPa * 100;
                         }
                         // Apply OpCost value limits
-                        if (nextStand.TotalSmLogTpa < nextStand.HarvestMethod.MinTpa)
+                        if (nextStand.TotalSmLogTpa > 0 && nextStand.TotalSmLogTpa < nextStand.HarvestMethod.MinTpa)
                         {
                             nextStand.TotalSmLogTpaUnadj = nextStand.TotalSmLogTpa;
                             nextStand.TotalSmLogTpa = nextStand.HarvestMethod.MinTpa;
@@ -682,7 +682,7 @@ namespace FIA_Biosum_Manager
                             dblLgLogHwdPct = nextStand.LgLogHwdVolCfPa / nextStand.LgLogVolCfPa * 100;
                         }
                         // Apply OpCost value limits
-                        if (nextStand.TotalLgLogTpa < nextStand.HarvestMethod.MinTpa)
+                        if (nextStand.TotalLgLogTpa > 0 && nextStand.TotalLgLogTpa < nextStand.HarvestMethod.MinTpa)
                         {
                             nextStand.TotalLgLogTpaUnadj = nextStand.TotalLgLogTpa;
                             nextStand.TotalLgLogTpa = nextStand.HarvestMethod.MinTpa;
